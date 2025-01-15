078331daee8701f768b22157d49943cfb0b85479
Touhiduzzaman Pavel <75662120+TouhidPavel@users.noreply.github.com>
2025-01-15 22:00:50 +0600

    Code Environment with Tailwind CSS

---
+++ React-Code-Environment/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <h1 className="bg-orange-600 text-white text-3xl py-5 font-bold text-center">
+                React v19
+            </h1>
+        </>
+    );
+}
+
+export default App;
